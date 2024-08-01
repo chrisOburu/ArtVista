24ca000e2ca3b00a126880d056df33feb2749874
chrisOburu <chrizoburu65@gmail.com>
2024-08-01 16:32:17 +0300

    modify app.py

--- server/app.py
+++ server/app.py
@@ -30,34 +30,38 @@ def index():
     return "<h1>Art Vista App</h1>"
 
 
-@app.route('/register', methods=['POST'])
-def register():
-    data = request.get_json()
-    username = data.get('username')
-    email = data.get('email')
-    password = data.get('password')
-    
-    if User.query.filter_by(email=email).first():
-        return jsonify({"message": "User already exists"}), 400
-    
-    user = User(username=username, email=email)
-    user.set_password(password)
-    
-    db.session.add(user)
-    db.session.commit()
-    
-    return jsonify({"message": "User registered successfully"}), 201
+class Register(Resource):
+    def post(Resource):
+        data = request.get_json()
+        username = data.get('username')
+        email = data.get('email')
+        password = data.get('password')
+        
+        if User.query.filter_by(email=email).first():
+            return jsonify({"message": "User already exists"}), 400
+        
+        user = User(username=username, email=email)
+        user.set_password(password)
+        
+        db.session.add(user)
+        db.session.commit()
+        
+        return jsonify({"message": "User registered successfully"}), 201
 
-@app.route('/login', methods=['POST'])
-def login():
-    data = request.get_json()
-    email = data.get('email')
-    password = data.get('password')
-    
-    user = User.query.filter_by(email=email).first()
-    
-    if user is None or not user.check_password(password):
-        return jsonify({"message": "Invalid credentials"}), 401
-    
-    access_token = create_access_token(identity=user.id)
-    return jsonify({"access_token": access_token}), 200
+
+class Login(Resource):
+    def post(Resource):
+        data = request.get_json()
+        email = data.get('email')
+        password = data.get('password')
+        
+        user = User.query.filter_by(email=email).first()
+        
+        if user is None or not user.check_password(password):
+            return jsonify({"message": "Invalid credentials"}), 401
+        
+        access_token = create_access_token(identity=user.id)
+        return jsonify({"access_token": access_token}), 200
+
+api.add_resource(Login, "/login")
+api.add_resource(Register, "/register")

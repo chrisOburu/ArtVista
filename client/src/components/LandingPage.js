f978e37edfca5dc679950cad992b43826ce8c96f
nunjayi <epenyash@gmail.com>
2024-08-10 12:48:55 +0300

    add header and footer kwa landing page

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -2,7 +2,8 @@ import React, { useState }  from 'react';
 import { Link } from 'react-router-dom';
 import LoginForm from './LoginForm';
 import Modal from 'react-modal';
-
+import Header from './header';
+import Footer from './footer';
 
 const LandingPage = ({onLoginSuccess}) => {
 
@@ -20,23 +21,12 @@ const LandingPage = ({onLoginSuccess}) => {
   
 
   return (
-    <div>
-      <header>
-        <h1>Welcome to ArtVista</h1>
-        <p>Discover inspiring projects and showcase your work.</p>
-        <button onClick={openModal}>Login</button>
-        <Link to="/register">Sign Up</Link>
-        <Link to="/projects">View Projects</Link>
-        <LoginForm
-          isOpen={modalIsOpen}
-          onRequestClose={closeModal}
-          onLoginSuccess={onLoginSuccess}
-          //onSuccess={handleLogin}
-          
-        />
-      </header>
-      {/* Add Project Grid and other content */}
-    </div>
+    <>
+      <Header />
+      <Footer />
+    </>
+
+
   );
 };
 

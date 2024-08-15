6edb1732966fb8f5daf0be00ce99b70fc216cc80
nunjayi <epenyash@gmail.com>
2024-08-15 20:57:03 +0300

    getting the current user

--- client/src/components/usernav.js
+++ client/src/components/usernav.js
@@ -14,7 +14,7 @@ function User(){
             <nav id='user'>
                 <a href="/" className="header-nav"><i class="bi bi-house-check-fill"></i>|Home</a>
                 <a href="/submit-project" className="header-nav"><i className="bi bi-folder-plus"></i>|project</a>
-                <img onClick ={()=>{navigate()}} src="https://images.unsplash.com/photo-1721332149274-586f2604884d?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" id="user-image" alt="" />
+                <img onClick={()=>{navigate('/profile')}} src="https://images.unsplash.com/photo-1721332149274-586f2604884d?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" id="user-image" alt="" />
             </nav>
         </>
     )
